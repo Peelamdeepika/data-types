b55033c49e537f76bd4a944507b5a3a5043d1053
Peelamdeepika <deepikapeelam16@gmail.com>
2025-12-09 18:28:42 +0530

    testing

--- typingdata.sh
+++ typingdata.sh
@@ -4,4 +4,4 @@ MOVIES=("Pusha" "kantara" "RRR")
 
 echo "first hit movie : ${MOVIES[0]}"
 echo "second hit movie : ${MOVIES[1]}"
-echo "Third hit movie : ${MOVIES[3]}"
+echo "Third hit movie : ${MOVIES[2]}"
