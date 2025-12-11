ee9a3e37bbc95c0d6deb572154ae61c032a3ba73
Peelamdeepika <deepikapeelam16@gmail.com>
2025-12-11 23:24:04 +0530

    condition added

---
+++ condition.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+USERID=$ (id -u)
+
+if [ $USERID -ne 0 ]; then
+echo "you need root access"
+
+yum install git -y
+fi
+
+if  [ $? -ne 0 ]; then
+
+echo "git not installed"
+
+else
+
+echo "git got successfully installed"
