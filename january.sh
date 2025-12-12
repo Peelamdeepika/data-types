f72f0cc8edd3d7448510a12c7e1cb37badc5b042
Peelamdeepika <deepikapeelam16@gmail.com>
2025-12-12 19:06:31 +0530

    added

--- january.sh
+++ january.sh
@@ -6,28 +6,32 @@ TIME_STAMP=$(date +%y-%m-%d)
 FILE=JANUARY
 FILE_NAME="$FOLDER/$FILE-$TIME_STAMP.log"
 
-
-Validate() {
-  if [ $1 -ne 0 ]; then
-   echo -e "$2... $R not installed"
-   exit 1
-   else
-   echo -e "$2... $G installed successfully"
-  fi 
-}
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 
+Validate() {
+  if [ $1 -ne 0 ]; then
+   echo -e "$2... has not  $R installed"
+   exit 1
+   else
+   echo -e "$2... has installed  $Y successfully"
+  fi 
+}
+ 
+if [ $USERID -eq 0 ]; then
+  echo "$R you need root access"
+  exit 1
+fi  
+
 for PACKAGE in $@
 do
-   dnf list $PACKAGE 
-   if [ $PACKAGE -ne 0 ]; then
-      echo " installing $PACKAGE"
-      dnf install $PACKAGE
+   dnf list installed $PACKAGE 
+   if [ $? -ne 0 ]; then
+      echo " now installing $PACKAGE"
+      dnf install $PACKAGE -y
       Validate $? "$PACKAGE"
     else
-     echo "$PACKAGE is already installed" 
+     echo "$PACKAGE is already installed in your server" 
     fi
 done      
