82e115d980ce30948b2dc1b8f87ab5426d51d6f4
Peelamdeepika <deepikapeelam16@gmail.com>
2025-12-12 15:12:46 +0530

    condition added

--- colours.sh
+++ colours.sh
@@ -3,19 +3,19 @@
 USERID=&(id -u)
 
 Vlaidate() {
-   if [ $1 -ne 0 ]; then
+  if [ $1 -ne 0 ]; then
    echo -e "$2... $R not installed"
    exit 1
-else
+   else
    echo -e "$2... $G installed successfully"
-fi 
+  fi 
 }
 
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 
-if [ $USERID -ne 0 ]; then
+if [ $USERID -ne 0 ] then
     echo -e "$R you need root access"
     exit 1
 fi
